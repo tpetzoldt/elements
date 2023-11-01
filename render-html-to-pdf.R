319e50d0b849984fd5ade1adce7c5179d08f2daf
tpetzoldt <thomas.petzoldt@tu-dresden.de>
2023-11-01 10:20:50 +0100

    numbering in render-html-to-pdf.R

--- render-html-to-pdf.R
+++ render-html-to-pdf.R
@@ -9,12 +9,13 @@ to_pdf("04-distributions.html")
 to_pdf("05-classtests.html")
 to_pdf("06-linear.html")
 to_pdf("07-anova.html")
-to_pdf("07-nonlinear-regression.html")
+to_pdf("08-nonlinear-regression.html")
 to_pdf("09-timeseries-basics.html")
 
 to_pdf("x1-r-basics.html")
 to_pdf("x2-r-graphics.html")
 to_pdf("x3-r-functions.html")
+to_pdf("x4-pipes-intro.html")
 
 #to_pdf("x4-pipes-intro.html") # does not work completely, has long slides
 
