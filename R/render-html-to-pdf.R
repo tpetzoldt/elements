fdeee2237503a3b56a3fc3adae13c1d74b155920
tpetzoldt <thomas.petzoldt@tu-dresden.de>
2023-11-05 21:20:06 +0100

    render script

--- R/render-html-to-pdf.R
+++ R/render-html-to-pdf.R
@@ -1,25 +1,28 @@
-# remotes::install_github('jhelvy/renderthis')
-# remotes::install_github('rstudio/chromote')
+#remotes::install_github('jhelvy/renderthis')
+#remotes::install_github('rstudio/chromote')
 #
 library("renderthis")
-to_pdf("01-introduction.html")
-to_pdf("02-terminology.html")
-to_pdf("03-statparams.html")
-to_pdf("04-distributions.html")
-to_pdf("05-classtests.html")
-to_pdf("06-linear.html")
-to_pdf("07-anova.html")
-to_pdf("08-nonlinear-regression.html")
-to_pdf("09-timeseries-basics.html")
+library("chromote")
 
-to_pdf("x1-r-basics.html")
-to_pdf("x2-r-graphics.html")
-to_pdf("x3-r-functions.html")
-to_pdf("x4-pipes-intro.html")
+to_pdf("https://tpetzoldt.github.io/elements/slides/01-introduction.html")
+to_pdf("https://tpetzoldt.github.io/elements/slides/02-terminology.html")
+to_pdf("https://tpetzoldt.github.io/elements/slides/03-statparams.html")
+to_pdf("https://tpetzoldt.github.io/elements/slides/04-distributions.html")
+to_pdf("https://tpetzoldt.github.io/elements/slides/05-classtests.html")
+to_pdf("https://tpetzoldt.github.io/elements/slides/06-linear.html")
+to_pdf("https://tpetzoldt.github.io/elements/slides/07-anova.html")
+to_pdf("https://tpetzoldt.github.io/elements/slides/08-nonlinear-regression.html")
+to_pdf("https://tpetzoldt.github.io/elements/slides/09-timeseries-basics.html")
+
+to_pdf("https://tpetzoldt.github.io/elements/slides/x1-r-basics.html")
+to_pdf("https://tpetzoldt.github.io/elements/slides/x2-r-graphics.html")
+to_pdf("https://tpetzoldt.github.io/elements/slides/x3-r-functions.html")
+to_pdf("https://tpetzoldt.github.io/elements/slides/x4-pipes-intro.html")
 
 #to_pdf("x4-pipes-intro.html") # does not work completely, has long slides
 
-
+## or directly from the web:
+#to_pdf("https://tpetzoldt.github.io/elements/slides/01-introduction.html")
 
 # alternative way:
 
